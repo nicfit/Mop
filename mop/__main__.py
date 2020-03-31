14278d32b991b549b2a4c0c9b41968b747d78e0b
Travis Shirk <travis@pobox.com>
2020-03-30 20:03:16 -0600

    new: Logging and version cli options via nicfit.py

--- mop/__main__.py
+++ mop/__main__.py
@@ -2,21 +2,26 @@ import sys
 import pathlib
 import logging
 import argparse
+from nicfit.logger import addCommandLineArgs as addLoggingArgs
 from .app import MopApp
+from .__about__ import version
 
 
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self):
-        super().__init__()
+        super().__init__(prog="mop")
         self._initArgs()
 
     def _initArgs(self):
+        self.add_argument("--version", action="version", version=f"%(prog)s {version}")
+        addLoggingArgs(self)
         self.add_argument("path_args", nargs="*", metavar="PATH", type=pathlib.Path,
                           help="An audio file or directory of audio files.")
 
 
 def main():
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
     cli = ArgumentParser()
     args = cli.parse_args()
 
