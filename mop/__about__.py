5e55c4eff6a21a88810c0d6531310c38b6e4ab2a
Travis Shirk <travis@pobox.com>
2020-04-05 18:53:13 -0600

    chg: dev: version bump

--- mop/__about__.py
+++ mop/__about__.py
@@ -1,8 +1,8 @@
 import dataclasses
 
 project_name = "Mop"
-version      = "0.1"
-release_name = "Like Rats"
+version      = "0.1.1"
+release_name = "Avalanche Master Song"
 author       = "Travis Shirk"
 author_email = "travis@pobox.com"
 years        = "2020"
@@ -15,4 +15,4 @@ class Version:
     release: str
     release_name: str
 
-version_info = Version(0, 1, 0, "final", "Like Rats")
+version_info = Version(0, 1, 1, "final", "Avalanche Master Song")
